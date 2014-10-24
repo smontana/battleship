50806996213ce5f41202a27810e880fff9f38077
Steve <stephen.a.montana@gmail.com>
2014-10-23 23:14:23 -0500

    initial style and layout

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var grid;
+
+var grid_options = {
+  size: 10
+}
+
+function start() {
+  build_grid( grid_options );
+  randomly_place_ships()
+}
+
+function build_grid(options) {
+  var user_board;
+  var machine_board;
+
+}
+
+function randomly_place_ships() {
+
+}
