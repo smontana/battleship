596a8d9aded4f70e6af8571cd6a10d704f4197d0
Steve <stephen.a.montana@gmail.com>
2014-10-24 01:42:09 -0500

    10-23-2014

--- js/main.js
+++ js/main.js
@@ -13,8 +13,53 @@ function build_grid(options) {
   var user_board;
   var machine_board;
 
+  var board_size = options['size']
+
+  var i = 0;
+	'<div data-x="' + '' + " data-y=" 'class="grid-cell"></div>'
+
 }
 
 function randomly_place_ships() {
 
+};
+
+
+
+
+//slightly redone
+
+var grid;
+
+var grid_options = {
+	size: 4
 }
+
+function start() {
+	build_grid( grid_options);
+	randomly_place_ships();
+}
+
+ function build_grid(options) {
+	size=4;
+
+	var cells = " ";
+	var i = 0;
+
+	while(i <= size) {
+
+		i2 = 0;
+
+		while(i2 <= size) {
+			cells += '<div data-x="' + i + '" data-y="' + i2 + '" class="grid-cell"></div>';
+			i2++;
+		}
+
+		i++;
+	}
+
+	return cells;
+};
+
+function randomly_place_ships() {
+	var position = Math.floor(Math.random() *)
